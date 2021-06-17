63cac1bfee6ab74163bab1fe26fe23bdea5d47e0
m_aykurt <maykurt92@gmail.com>
2021-06-17 14:23:32 +0300

    fs-js

---
+++ 2-JAVASCRİPT/In-class/2-lesson-2 variable/app.js
@@ -0,0 +1,13 @@
+// console.log("hello clarusway");
+// console.info("some infor");
+// console.warn("some warning");
+// console.error("error!!!");
+// console.log("merhaba");
+
+console.log("----------");
+
+var myName= "murat";
+console.log(myName);
+
+// myNema= "hasan";
+// console.log(myName);
