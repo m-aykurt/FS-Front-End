d2ce8bed6c21b814fa1344b6f04d351d6852d330
m_aykurt <maykurt92@gmail.com>
2021-06-21 16:10:11 +0300

    comm-1

---
+++ 2-JAVASCRİPT/Pre-class/2-variables/app.js
@@ -0,0 +1,13 @@
+var isim = prompt("merhabalar lütfen isim giriniz: ");
+alert("hosgeldiniz "+isim);
+var dogumTarihi = prompt("dogum tarihinizi giriniz: ");
+var yas;
+yas = 2021 - dogumTarihi;
+
+alert("merhaba "+isim+", "+yas+ " yasindasiniz")
+console.log(isim+yas);
+
+let x=3;
+let y=5;
+
+if (x==y) {alert("x - y eşit değil")} else {alert("x y eşit DEĞİLDİR")};
