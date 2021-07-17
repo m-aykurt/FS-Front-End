9c27fa7955dd0c511f427716f1883cb7819c6cf5
m_aykurt <maykurt92@gmail.com>
2021-07-17 12:55:41 +0300

    added full proj

--- 2-JAVASCRİPT/2-Pre-class/8-string-numbers-methods/num-app.js
+++ 2-JAVASCRİPT/2-Pre-class/8-string-numbers-methods/num-app.js
@@ -1,8 +1,29 @@
-var num = 15.123456789;
+// var num = 15.123456789;
 
-val = num.toPrecision(3);
-val = num.toFixed(3);
-val = Math.round(num)
+// val = num.toPrecision(3);
+// val = num.toFixed(3);
+// val = Math.round(num)
+// val = Math.floor(num)
+// val = Math.ceil(num)
+// val = Math.max(1, 2, 10, 56, 20)
+// val = Math.min(1, 2, 10, 56, 20)
+// val = Math.min(1, 2, 10, 56, 20)
 
+// var min = 50;
+// var max = 100;
+// val = (Math.floor(min + Math.random() * (max-min)));
+// // console.log(val);
 
-console.log(val);
+// // maaş hesabı
+
+// let brutMaas = 3700;
+// let mesaiUcreti = 10.3;
+// var mesaiSuresi = 42;
+//  var toplamBrutMaas = brutMaas + (mesaiUcreti*mesaiSuresi)
+
+//  var toplamNetMaas = toplamBrutMaas - toplamBrutMaas*0.25
+
+//  console.log(toplamBrutMaas.toFixed(3));
+//  console.log(typeof toplamBrutMaas.toFixed(3));
+//  console.log(toplamNetMaas.toFixed(3));
+ 
