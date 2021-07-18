6298044f2d529e2fbe7d96bd380c4a0389bc4c5d
m_aykurt <maykurt92@gmail.com>
2021-07-18 13:54:12 +0300

    Completed calculator proj. in class

---
+++ 3-Projects/0-Odin Projects/0-functions/app.js
@@ -0,0 +1,27 @@
+let myArray = ['I', 'love', 'chocolate', 'frogs'];
+let madeAString = myArray.join('-');
+console.log(madeAString);
+// the join() function takes an array, joins
+// all the array items together into a single
+// string, and returns this new string
+
+let myText = 'I am a string';
+let newString = myText.replace('string', 'sausage');
+console.log(newString);
+// the replace() string function takes a source string,
+// and a target string and replaces the source string,
+// with the target string, and returns the newly formed string
+
+// function myFunction() {
+//     alert('hello');
+//   }
+  
+//   myFunction();
+
+// const myGreeting = function() {
+//     alert('hello');
+//   }
+
+// //   myGreeting();
+// let anotherGreeting = myGreeting;
+// anotherGreeting()
