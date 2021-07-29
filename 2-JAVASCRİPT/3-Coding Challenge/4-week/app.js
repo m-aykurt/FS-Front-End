64edc7277d7d784c471696dfb7bf12298fb749f7
m_aykurt <maykurt92@gmail.com>
2021-07-29 08:17:53 +0300

    added odin

---
+++ 2-JAVASCRİPT/3-Coding Challenge/4-week/app.js
@@ -0,0 +1,14 @@
+// let arrayA = [1, 2, 3, 6, 7]
+// let arrayB = [2, 3, 5, 7]
+// let c = []
+
+// function mergeArrays(arrayA, arrayB) {
+//     c = arrayA + arrayB
+//     console.log(c);
+// }
+
+// mergeArrays()
+
+var iller='["İstanbul","Ankara","İzmir","Edirne"]';
+var liste = JSON.parse(iller);
+alert(liste); //İstanbul,Ankara,İzmir,Edirne
--- 2-JAVASCRİPT/3-Coding Challenge/Slack-js/app.js
+++ 2-JAVASCRİPT/3-Coding Challenge/Slack-js/app.js
@@ -1,24 +1,22 @@
-
-
 // vowel problem
 
-function vowelFinder() {
-    const sentence = prompt("Enter a sentence: ").toLocaleLowerCase();
-    // console.log(sentence)
-    const splitted = sentence.split("");
-    let unique = splitted.filter((item, i, ar) => ar.indexOf(item) === i);
-    const vowels = ["a", "e", "ı", "i", "o", "ö", "u", "ü"];
-    let vowelList = [];
-    for (i = 0; i < unique.length; i++) {
-        if (vowels.includes(unique[i])) {
-            vowelList.push(unique[i])
-        }
-    }
-    console.log(`The sentence you entered is: ${sentence}`)
-    console.log(`The vowels in the sentence are: ${vowelList}`);
-    console.log(`Total number of vowels: ${ vowelList.length}`);
-}
-vowelFinder();
+// function vowelFinder() {
+//     const sentence = prompt("Enter a sentence: ").toLocaleLowerCase();
+//     // console.log(sentence)
+//     const splitted = sentence.split("");
+//     let unique = splitted.filter((item, i, ar) => ar.indexOf(item) === i);
+//     const vowels = ["a", "e", "ı", "i", "o", "ö", "u", "ü"];
+//     let vowelList = [];
+//     for (i = 0; i < unique.length; i++) {
+//         if (vowels.includes(unique[i])) {
+//             vowelList.push(unique[i])
+//         }
+//     }
+//     console.log(`The sentence you entered is: ${sentence}`)
+//     console.log(`The vowels in the sentence are: ${vowelList}`);
+//     console.log(`Total number of vowels: ${ vowelList.length}`);
+// }
+// vowelFinder();
 
 
 // let  vowel_count = function(str)
@@ -26,14 +24,14 @@ vowelFinder();
 //   var vowel_list = 'aeiouıöü';
 //   var vcount = 0;
 //   var str1 = str.toLowerCase().trim().replace(" ","")
-  
+
 //   for(var i = 0; i < str1.length ; i++)
 //   {
 //     if (vowel_list.indexOf(str1[i]) !== -1)
 //     {
 //       vcount += 1;
 //     }
-  
+
 //   }
 //   let lng = `vowel length : ${vcount}, cons length: ${str1.length-vcount}`
 //   return lng;
@@ -46,12 +44,38 @@ vowelFinder();
 
 // fahrenait-to celcius
 
-const heat = prompt("Please select CF OR FC").toLocaleLowerCase();
-const num = Number(prompt("Please enter to number for convert !"));
-if (heat === "cf"){
-    console.log(`${num} Celcius = `,  ((num * 9/5)+32).toFixed(2), `Fahrenhayt`);
-}else if (heat === "fc") {
-    console.log(`${num} Fahrenhayt = `, ((num-32)*5/9).toFixed(2), `Celcius`);
-}else{
-    console.log("please enter valid value !");
+// const heat = prompt("Please select CF OR FC").toLocaleLowerCase();
+// const num = Number(prompt("Please enter to number for convert !"));
+// if (heat === "cf"){
+//     console.log(`${num} Celcius = `,  ((num * 9/5)+32).toFixed(2), `Fahrenhayt`);
+// }else if (heat === "fc") {
+//     console.log(`${num} Fahrenhayt = `, ((num-32)*5/9).toFixed(2), `Celcius`);
+// }else{
+//     console.log("please enter valid value !");
+// }
+
+// ODİN PROJECT JS FUNCTİON
+
+// let myArray = ['I', 'love', 'chocolate', 'frogs'];
+// let madeAString = myArray.join(' ');
+// console.log(madeAString);
+// the join() function takes an array, joins
+// all the array items together into a single
+// string, and returns this new string
+
+
+// prob-1 fizz-buzz
+
+let answer = parseInt(prompt("enter a number: "));
+
+for (let i = 1; i <= answer; i++) {
+    if ((!(i % 3)) && (!(i % 5))) {
+        console.log("FIZZ BUZZ");
+    } else if (!(i % 5)) {
+        console.log("BUZZ");
+    } else if (!(i % 3)) {
+        console.log("FIZZ");
+    } else {
+        console.log(i);
+    }
 }
