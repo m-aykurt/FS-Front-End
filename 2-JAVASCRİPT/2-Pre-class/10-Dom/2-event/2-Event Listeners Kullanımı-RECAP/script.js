1c3cac4b94e4bf78067e7b69a4a7614e566f0d6a
m_aykurt <maykurt92@gmail.com>
2021-07-30 18:09:57 +0300

    4-proj

--- 2-JAVASCRİPT/2-Pre-class/10-Dom/2-event/2-Event Listeners Kullanımı-RECAP/script.js
+++ 2-JAVASCRİPT/2-Pre-class/10-Dom/2-event/2-Event Listeners Kullanımı-RECAP/script.js
@@ -1,33 +1,92 @@
 // Event Listeners
 
-const btn = document.querySelector('#btnDeleteAll');
-const btn2 = document.querySelector('#btnAddNewTask');
+// const btn = document.querySelector('#btnDeleteAll');
+// const btn2 = document.querySelector('#btnAddNewTask');
 
-btn.addEventListener('click',function(e){
-  
-    let val;
+// btn.addEventListener('click',function(e){
 
-    val = e;
+//     let val;
 
-    val = e.target;
-    val = e.target.id;
-    val = e.target.classList;
-    val = e.type;
+//     val = e;
 
-    console.log(val);
+//     val = e.target;
+//     val = e.target.id;
+//     val = e.target.classList;
+//     val = e.type;
 
-    e.preventDefault();
+//     console.log(val);
 
-});
+//     e.preventDefault();
 
-// btn.addEventListener('click',btnClick);
-// btn.addEventListener('click',btnClick2);
-// btn2.addEventListener('click',btnClick);
+// });
+
+// prevent default
+// btn.addEventListener("click",function(e){
+//     // console.log("btn-clicked");
+
+//     let val;
+//     val  = e;
+//     val  = e.target;
+//     val  = e.target.id;
+//     val  = e.target.classList;
+//     val  = e.type;
+
+
+//     console.log(val);
+//     e.preventDefault();
+// });
 
 // function btnClick(){
-//     console.log('btn clicked');
+//     console.log("btn clicked");
 // }
 
 // function btnClick2(){
-//     console.log('btn 2 clicked');
+//     console.log("btn clicked2");
 // }
+
+
+
+// MOUSE EVENTS
+
+const btn = document.querySelector("#btnDeleteAll");
+const ul = document.querySelector("#task-list");
+
+// click
+
+// btn.addEventListener("click", eventHandler);
+// ul.addEventListener("click",eventHandler)
+
+
+//double click
+// btn.addEventListener("dblclick",eventHandler)
+
+// mouse down
+// btn.addEventListener("mousedown",eventHandler)
+// mouse up
+// btn.addEventListener("mouseup",eventHandler)
+
+//mouse enter
+
+// ul.addEventListener("mouseenter",eventHandler);
+
+// // mouse leave
+
+// ul.addEventListener("mouseleave",eventHandler);
+
+// //mouseover
+// ul.addEventListener("mouseover",eventHandler)
+// //mouseout
+// ul.addEventListener("mouseout",eventHandler)
+
+
+// mouse move
+
+ul.addEventListener("mousemove",eventHandler);
+
+function eventHandler(event) {
+    // event.preventDefault();
+    console.log(`EVENT TYPE: ${event.type}`);
+    // console.log(event);
+    h5.textContent = `mouse x`
+
+}
